65d1dc4c1a6ea0a6d092119e3da6c2eefb1d0d6f
seanwhs <54506937+seanwhs@users.noreply.github.com>
2025-10-31 19:41:13 +0800

    Create function_builder.py

---
+++ function_builder.py
@@ -0,0 +1,9 @@
+# Function Builder using lambda
+def funcBuilder(x):
+    return lambda num: num + x
+
+addTen = funcBuilder(10)
+addTwenty = funcBuilder(20)
+
+print(addTen(7))
+print(addTwenty(7))
